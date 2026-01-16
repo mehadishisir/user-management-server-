f0d18ddc1b3d80be09ecc5df1a2c1ebeaedf7e72
mehadi <mehadishisir@gmail.com>
2026-01-16 10:45:38 +0600

    make a api and use it on client site

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = 3000;
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("hello world how are you?    ");
+});
+
+app.get("/users", (req, res) => {
+  res.send([
+    { name: "John", age: 30 },
+    { name: "Jane", age: 25 },
+    { name: "Doe", age: 40 },
+  ]);
 });
 app.listen(port);
